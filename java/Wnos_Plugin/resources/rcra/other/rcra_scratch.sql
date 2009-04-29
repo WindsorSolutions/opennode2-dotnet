d57390abc8abbf2d0e99f214844b5f36cb0db4b3
dissolving <dissolving@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2009-04-29 19:11:26 +0000

    initial release of v10
    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@39 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

---
+++ java/Wnos_Plugin/resources/rcra/other/rcra_scratch.sql
@@ -0,0 +1,5 @@
+select count (*) from RCRA_HD_HBASIC
+
+select * from RCRA_HD_HBASIC WHERE row_number <= 10
+
+select * from RCRA_HD_HBASIC FETCH FIRST 10 ROWS ONLY
---
+++ java/Wnos_Plugin/resources/velocity-plugin-helper/run.sh
@@ -0,0 +1,24 @@
+CP=${CLASSPATH}
+
+# Set path to the Java SDK
+JDK=${JAVA_HOME}
+CP=${CP}:${JDK};
+
+# add folders to path
+CP=${CP}:conf;
+
+# Set path to all the jars in lib
+for lib in lib/*.jar; do
+    CP=${CP}:${lib};
+done
+
+#for lib in bin/*.jar; do
+#    CP=${CP}:${lib};
+#done
+
+
+# Set main
+MAINCLASS=com.windsor.node.plugin.common.velocity.ConsoleVelocityHelper
+
+# Go
+${JDK}/bin/java -classpath ${CP} ${MAINCLASS} "conf/context_config.xml"
