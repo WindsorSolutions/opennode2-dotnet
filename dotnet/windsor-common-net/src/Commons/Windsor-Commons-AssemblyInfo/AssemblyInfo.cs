334e53e19a8440a5ae4cd95c80df14a1ca70296c
tedsmorris <tedsmorris@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2011-08-31 22:49:36 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@318 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

--- dotnet/windsor-common-net/src/Commons/Windsor-Commons-AssemblyInfo/AssemblyInfo.cs
+++ dotnet/windsor-common-net/src/Commons/Windsor-Commons-AssemblyInfo/AssemblyInfo.cs
@@ -44,10 +44,10 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
 	{
 		// [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "1.0.1.315";
+        public const string cAssemblyVersion = "1.0.1.317";
 		
 		// [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
-		public const string cAssemblyFileVersion = "1.0.1.315";
+		public const string cAssemblyFileVersion = "1.0.1.317";
 
 		// [assembly: AssemblyCompany(AssemblyInfoServer.cAssemblyCompany)]
 		public const string cAssemblyCompany = "Windsor Solutions, Inc."; 
