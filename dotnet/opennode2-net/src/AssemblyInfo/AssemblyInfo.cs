62ae2c8e5d05e03469c3eb3f49914da1305e4cf1
tedsmorris <tedsmorris@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2011-09-01 18:21:09 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@321 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

--- dotnet/opennode2-net/src/AssemblyInfo/AssemblyInfo.cs
+++ dotnet/opennode2-net/src/AssemblyInfo/AssemblyInfo.cs
@@ -44,10 +44,10 @@ namespace Windsor.Node2008.WNOS.AssemblyInfo
     internal static class AssemblyInfo
 	{
 		// [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "1.2.1.319";
+        public const string cAssemblyVersion = "1.2.1.320";
 		
 		// [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
-		public const string cAssemblyFileVersion = "1.2.1.319";
+		public const string cAssemblyFileVersion = "1.2.1.320";
 
 		// [assembly: AssemblyCompany(AssemblyInfoServer.cAssemblyCompany)]
 		public const string cAssemblyCompany = "Windsor Solutions, Inc."; 
