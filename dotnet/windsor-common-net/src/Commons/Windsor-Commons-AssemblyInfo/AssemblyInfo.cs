6ea016076d71ccef4c747c78396ac8171ad61ed3
tedsmorris <tedsmorris@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2011-11-29 01:45:52 +0000

    eEMTS v2 decimal prec updates
    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@353 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

--- dotnet/windsor-common-net/src/Commons/Windsor-Commons-AssemblyInfo/AssemblyInfo.cs
+++ dotnet/windsor-common-net/src/Commons/Windsor-Commons-AssemblyInfo/AssemblyInfo.cs
@@ -44,10 +44,10 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
 	{
 		// [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "1.0.1.343";
+        public const string cAssemblyVersion = "1.0.1.351";
 		
 		// [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
-		public const string cAssemblyFileVersion = "1.0.1.343";
+		public const string cAssemblyFileVersion = "1.0.1.351";
 
 		// [assembly: AssemblyCompany(AssemblyInfoServer.cAssemblyCompany)]
 		public const string cAssemblyCompany = "Windsor Solutions, Inc."; 
