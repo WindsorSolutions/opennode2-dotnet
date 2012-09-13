a63384ab8f4a68d6604f73d0c4037964b10b96e7
andrew_geery@windsorsolutions.com <andrew_geery@windsorsolutions.com@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2012-09-13 01:24:52 +0000

    Added a check that the transaction type in the staging tables is not null for the data being submitted.
    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@561 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

--- java/node-plugin/src/main/java/com/windsor/node/plugin/icisnpdes40/submission/PerformICISSubmission.java
+++ java/node-plugin/src/main/java/com/windsor/node/plugin/icisnpdes40/submission/PerformICISSubmission.java
@@ -113,7 +113,7 @@ public class PerformICISSubmission extends AbstractIcisNpdesSubmission {
                  * Use the class name to create a JPQL select statement and then
                  * get the results.
                  */
-                final List<?> list = em.createQuery("select ls from "+klassName+" ls").getResultList();
+                final List<?> list = em.createQuery("select ls from "+klassName+" ls where ls.transactionHeader.transactionType is not null").getResultList();
                 
                 log("...Found {} records in the database.", list.size());
                 
