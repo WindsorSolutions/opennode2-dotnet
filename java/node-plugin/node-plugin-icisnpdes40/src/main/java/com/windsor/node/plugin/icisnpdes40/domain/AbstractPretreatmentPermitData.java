406b66646f500a6ddd3336e2230379c9e898d93b
andrew_geery@windsorsolutions.com <andrew_geery@windsorsolutions.com@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2012-11-02 00:00:01 +0000

    Fixed a bug where non-existent contacts for master general permits and pretreatment permits were generating illegal empty contact-related elements.
    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@665 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

---
+++ java/node-plugin/node-plugin-icisnpdes40/src/main/java/com/windsor/node/plugin/icisnpdes40/domain/AbstractPretreatmentPermitData.java
@@ -0,0 +1,43 @@
+package com.windsor.node.plugin.icisnpdes40.domain;
+
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
+
+import com.windsor.node.plugin.icisnpdes40.generated.PretreatmentPermit;
+
+/**
+ * Specifies how to access contact info for {@link PretreatmentPermit} data.
+ *
+ */
+@MappedSuperclass
+public abstract class AbstractPretreatmentPermitData extends AbstractAddressContactEntity {
+
+	/**
+	 * Returns the {@link PretreatmentPermit} object. The generated class will
+	 * override this method.
+	 *
+	 * @return the {@link PretreatmentPermit} object
+	 */
+	@Transient
+	public abstract PretreatmentPermit getPretreatmentPermit();
+
+	@Override
+	@Transient
+	public IContactList getContactList() {
+		final PretreatmentPermit permit = getPretreatmentPermit();
+		IContactList contactList = null;
+		if (permit != null) {
+			contactList = permit.getPretreatmentContact();
+		}
+		return contactList;
+	}
+
+	@Override
+	public void nullContact() {
+		final PretreatmentPermit permit = getPretreatmentPermit();
+		if (permit != null) {
+			permit.setPretreatmentContact(null);
+		}
+	}
+
+}
