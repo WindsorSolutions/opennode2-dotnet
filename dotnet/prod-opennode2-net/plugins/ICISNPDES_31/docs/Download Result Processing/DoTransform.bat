6a33c49bc01342bfecb18d1d0745f806a434d31c
bill_rensmith@windsorsolutions.com <bill_rensmith@windsorsolutions.com@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2012-11-07 22:24:15 +0000

    ICIS: Update XSLT to account for undeclared PermitTerminationIdentifier in ICIS-NPDES XML Schema
    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@670 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

--- dotnet/prod-opennode2-net/plugins/ICISNPDES_31/docs/Download Result Processing/DoTransform.bat
+++ dotnet/prod-opennode2-net/plugins/ICISNPDES_31/docs/Download Result Processing/DoTransform.bat
@@ -1,4 +1,4 @@
 @echo off
 
-AltovaXML /xslt1 "MappingMapToSubmissionResults.xslt" /in "AllResponseXMLsCheck.xml" /out "SubmissionResults.xml" %*
+AltovaXML /xslt1 "MappingMapToSubmissionResults.xslt" /in "C:/google-opennode2/dotnet/prod-opennode2-net/plugins/ICISNPDES_40/docs/Download Result Processing/Rejected_Response_Termination.xml" /out "SubmissionResults.xml" %*
 IF ERRORLEVEL 1 EXIT/B %ERRORLEVEL%
