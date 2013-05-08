4d41d069ab20c66f2ed6b9e56784bb05c66a053c
ted_morris@windsorsolutions.com <ted_morris@windsorsolutions.com@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2013-05-08 21:32:07 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@806 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

--- dotnet/common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
+++ dotnet/common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "2.6.0.803";
+        public const string cAssemblyVersion = "2.6.0.805";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
