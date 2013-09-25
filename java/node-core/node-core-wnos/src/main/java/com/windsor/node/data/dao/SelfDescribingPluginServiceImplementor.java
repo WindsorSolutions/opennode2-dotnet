cbe1342c3ac43323e6529cb81692730de3c6525c
tyler_murdock@windsorsolutions.com <tyler_murdock@windsorsolutions.com@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2013-09-25 19:06:56 +0000

    Adding files that got held back for 2.08
    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@896 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

---
+++ java/node-core/node-core-wnos/src/main/java/com/windsor/node/data/dao/SelfDescribingPluginServiceImplementor.java
@@ -0,0 +1,8 @@
+package com.windsor.node.data.dao;
+
+import com.windsor.node.common.domain.PluginServiceImplementorDescriptor;
+
+public interface SelfDescribingPluginServiceImplementor
+{
+    PluginServiceImplementorDescriptor getPluginServiceImplementorDescription();
+}
