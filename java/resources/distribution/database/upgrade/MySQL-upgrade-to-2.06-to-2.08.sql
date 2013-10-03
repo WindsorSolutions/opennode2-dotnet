f745a2012adf6a39f4c3eb13f318a218cebccda5
tyler_murdock@windsorsolutions.com <tyler_murdock@windsorsolutions.com@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2013-10-03 21:00:59 +0000

    Update scripts
    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@911 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

---
+++ java/resources/distribution/database/upgrade/MySQL-upgrade-to-2.06-to-2.08.sql
@@ -0,0 +1,5 @@
+alter table NTransaction modify NetworkId varchar(255) null;
+update NPartner set Version = 'EN2.1' where Version = 'EN2.0';
+update NPartner set Version = 'EN2.1' where Version = 'Undefined';
+update NTransaction set EndpointVersion = 'EN2.1' where EndpointVersion = 'EN2.0';
+update NTransaction set NetworkEndpointVersion = 'EN2.1' where NetworkEndpointVersion = 'EN2.0';
