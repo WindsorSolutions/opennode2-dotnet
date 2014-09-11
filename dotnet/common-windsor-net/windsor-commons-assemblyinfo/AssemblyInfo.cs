91ec53f636563fcdab3c2558ad995ed66aa7d830
tedmorris <tedmorris@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2014-09-11 17:45:10 +0000

    ICIS Air updates
    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@960 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

--- dotnet/common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
+++ dotnet/common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "2.6.3.958";
+        public const string cAssemblyVersion = "2.6.4.959";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
--- dotnet/common-windsor-net/windsor-commons-assemblyinfo/_AssemblyInfo.cs
+++ dotnet/common-windsor-net/windsor-commons-assemblyinfo/_AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "2.6.3.$WCREV$";
+        public const string cAssemblyVersion = "2.6.4.$WCREV$";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
