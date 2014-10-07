958c929eea2decb69c9fc78583617dd6dbb5542f
tedmorris <tedmorris@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2014-10-07 00:17:07 +0000

    EMTS_30
    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@966 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

--- dotnet/common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
+++ dotnet/common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "2.6.6.964";
+        public const string cAssemblyVersion = "2.6.6.965";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
---
+++ dotnet/prod-opennode2-net/plugins/EMTS_30/xml_schema/EMTS_v3_0_2.cs
@@ -0,0 +1,70 @@
+using System.Xml.Serialization;
+using System.Data;
+using System;
+using Windsor.Commons.XsdOrm;
+using Windsor.Commons.Core;
+
+namespace Windsor.Node2008.WNOSPlugin.EMTS_30
+{
+
+    [DefaultTableNamePrefixAttribute("EMTS")]
+    [ShortenNamesByRemovingVowelsFirstAttribute]
+    [FixShortenNameBreakBugAttribute]
+    [InheritAppliedAttributesAttribute]
+    [DefaultDecimalPrecision(5, 2)]
+    [AdditionalAbbreviationsAttribute("REPORT", "RPT",
+                                      "SUPPORTING", "SUPRT",
+                                      "GENERATE", "GENR",
+                                      "FEEDSTOCK", "FDSTK",
+                                      "SEPARATE", "SEPR",
+                                      "PRODUCT", "PROD",
+                                      "PRODUCTION", "PROD",
+                                      "ASSIGNMENT", "ASSIGN",
+                                      "AMOUNT", "AMT",
+                                      "RENEWABLE", "RENEW",
+                                      "DOCUMENT", "DOC",
+                                      "PUBLIC", "PUB"
+    )]
+
+    [AppliedAttribute(typeof(FeedstockDetailDataType), "FeedstockQuantity", typeof(ColumnAttribute), 10, 1)]
+
+    [AppliedAttribute(typeof(GenerateTransactionDetailDataType), "EquivalenceValue", typeof(ColumnAttribute), 2, 1)]
+
+    [AppliedAttribute(typeof(SellTransactionDetailDataType), "RINPriceAmount", typeof(ColumnAttribute), 5, 2)]
+    [AppliedAttribute(typeof(SellTransactionDetailDataType), "GallonPriceAmount", typeof(ColumnAttribute), 5, 2)]
+
+    [AppliedAttribute(typeof(BuyTransactionDetailDataType), "RINPriceAmount", typeof(ColumnAttribute), 5, 2)]
+    [AppliedAttribute(typeof(BuyTransactionDetailDataType), "GallonPriceAmount", typeof(ColumnAttribute), 5, 2)]
+
+    public partial class EMTSDataType
+    {
+    }
+
+    public partial class SeparateSupportingDocumentDetailDataType : SupportingDocumentDetailDataType
+    {
+    }
+
+    public partial class SellSupportingDocumentDetailDataType : SupportingDocumentDetailDataType
+    {
+    }
+
+    public partial class BuySupportingDocumentDetailDataType : SupportingDocumentDetailDataType
+    {
+    }
+
+    public partial class RetireSupportingDocumentDetailDataType : SupportingDocumentDetailDataType
+    {
+    }
+
+    public partial class SellPublicSupportingDocumentDetailDataType : SupportingDocumentDetailDataType
+    {
+    }
+
+    public partial class BuyPublicSupportingDocumentDetailDataType : SupportingDocumentDetailDataType
+    {
+    }
+
+    public partial class GenerateSupportingDocumentDetailDataType : SupportingDocumentDetailDataType
+    {
+    }
+}
