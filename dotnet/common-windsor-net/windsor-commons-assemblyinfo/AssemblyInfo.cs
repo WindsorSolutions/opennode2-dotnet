207017b31e5fcfded0c2b81cbf428d6ec3190cba
tedmorris <tedmorris@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2015-01-13 23:07:47 +0000

    add missing reference
    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@1001 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

--- dotnet/common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
+++ dotnet/common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "2.6.8.989";
+        public const string cAssemblyVersion = "2.6.8.1000";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
