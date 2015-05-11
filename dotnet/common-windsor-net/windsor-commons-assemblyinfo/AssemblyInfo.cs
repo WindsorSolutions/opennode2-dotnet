3180c9eff8ee54cfd59ab5db4a2e79810b161ec3
tedmorris <tedmorris@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2015-05-11 15:59:49 +0000

    new icis-air and icis ddls
    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@1026 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

--- dotnet/common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
+++ dotnet/common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "2.6.9.1015";
+        public const string cAssemblyVersion = "2.6.9.1025";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
