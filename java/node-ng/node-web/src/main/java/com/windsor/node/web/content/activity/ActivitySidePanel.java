c7efcb19c3f96490d61573133f07093c35109e5d
cmiles74windsor <cmiles74windsor@a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4>
2017-09-01 14:55:09 +0000

    Added the ability to download server logs.
    git-svn-id: svn+ssh://svn.code.sf.net/p/opennode2/src/trunk@1321 a46bb6aa-65bc-42c6-9d02-ca0d7e7f78c4

--- java/node-ng/node-web/src/main/java/com/windsor/node/web/content/activity/ActivitySidePanel.java
+++ java/node-ng/node-web/src/main/java/com/windsor/node/web/content/activity/ActivitySidePanel.java
@@ -1,5 +1,14 @@
 package com.windsor.node.web.content.activity;
 
+import com.altova.io.FileInput;
+import com.windsor.node.domain.NodePermission;
+import com.windsor.node.service.props.NosProperties;
+import com.windsor.node.web.behavior.OnlyVisibleForPermissionsBehavior;
+import com.windsor.stack.web.wicket.behavior.AjaxDownload;
+import de.agilecoders.wicket.core.markup.html.bootstrap.button.BootstrapAjaxLink;
+import de.agilecoders.wicket.core.markup.html.bootstrap.button.Buttons;
+import org.apache.commons.io.IOUtils;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.basic.MultiLineLabel;
 import org.apache.wicket.markup.html.form.Form;
@@ -15,11 +24,35 @@ import com.windsor.stack.web.wicket.markup.html.form.button.SearchButton;
 import com.windsor.stack.web.wicket.model.IdentifiableResourceModel;
 
 import de.agilecoders.wicket.core.markup.html.bootstrap.form.FormGroup;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.util.resource.AbstractResourceStream;
+import org.apache.wicket.util.resource.IResourceStream;
+import org.apache.wicket.util.resource.ResourceStreamNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Provides a side panel.
  */
 public class ActivitySidePanel extends AbstractBasePanel<ActivitySearchCriteria> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ActivitySidePanel.class);
+
+    @SpringBean
+    private NosProperties nosProps;
+
+    private AjaxDownload download;
 
     public ActivitySidePanel(String id, IModel<ActivitySearchCriteria> model, IModel<String> contentModel) {
         super(id, model);
@@ -28,6 +61,55 @@ public class ActivitySidePanel extends AbstractBasePanel<ActivitySearchCriteria>
 
         Form<ActivitySearchCriteria> form = new Form<>("form", model);
         add(form);
+
+        download = new AjaxDownload() {
+            @Override
+            protected IResourceStream getResourceStream() {
+                return new AbstractResourceStream() {
+
+                    private File logZip;
+
+                    @Override
+                    public InputStream getInputStream() throws ResourceStreamNotFoundException {
+                        try {
+                            logZip = File.createTempFile("logs", ".zip");
+                            FileOutputStream fos = new FileOutputStream(logZip);
+                            ZipOutputStream zos = new ZipOutputStream(fos);
+                            File logDir = new File(nosProps.getLogDirectory());
+                            for (File f: logDir.listFiles()) {
+                                ZipEntry ze = new ZipEntry(f.getName());
+                                zos.putNextEntry(ze);
+                                FileInputStream in = new FileInputStream(f);
+                                IOUtils.copy(in, zos);
+                                IOUtils.closeQuietly(in);
+                            }
+                            zos.closeEntry();
+                            zos.close();
+                            return new FileInputStream(logZip);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
+                    @Override
+                    public void close() throws IOException {
+                        try {
+                            logZip.delete();
+                        } catch (Exception e) {
+                            LOGGER.warn("Error deleting temp log zip file", e);
+                        }
+                    }
+
+
+                };
+            }
+
+            @Override
+            protected String getFileName() {
+                return "opennode2_logs.zip";
+            }
+        };
+        add(download);
 
         form.add(new FormGroup("detailGroup")
                 .add(new TextField<>("field", ActivitySearchCriteriaModels.bindDetails(model))
@@ -44,6 +126,18 @@ public class ActivitySidePanel extends AbstractBasePanel<ActivitySearchCriteria>
         add(new MultiLineLabel("content", contentModel)
                 .setEscapeModelStrings(false)
                 .setOutputMarkupId(true));
+
+        add(new Label("logs-title", Model.of("Server Logs")));
+        add(new BootstrapAjaxLink<String>("logs", Buttons.Type.Primary) {
+
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                download.initiate(target);
+            }
+
+        }.setLabel(Model.of("Download"))
+                .add(new OnlyVisibleForPermissionsBehavior(NodePermission.ADMIN_USER)));
+
     }
 
 }
