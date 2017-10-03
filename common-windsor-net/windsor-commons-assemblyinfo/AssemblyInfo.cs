8804f871d078ff08cd8653b11694ddf95e033fcb
tedmorris <31996410+tedmorris@users.noreply.github.com>
2017-10-03 12:29:40 -0700

    added ATTAINS 1.0 xsd files

--- common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
+++ common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "4.0.5.1209";
+        public const string cAssemblyVersion = "4.0.6.1210";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
--- common-windsor-net/windsor-commons-assemblyinfo/_AssemblyInfo.cs
+++ common-windsor-net/windsor-commons-assemblyinfo/_AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "4.0.6.$WCREV$";
+        public const string cAssemblyVersion = "4.0.7.$WCREV$";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
