e41d040458b89ce5707e0314bfcf53a8fe6ea8b8
tedmorris <31996410+tedmorris@users.noreply.github.com>
2017-11-06 19:33:09 -0800

    Start RCRA 5.5

--- common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
+++ common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "4.0.8.1220";
+        public const string cAssemblyVersion = "4.0.8.1221";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
