a39505fe5ad715bc3c8a577bcf5caeab6c7d8f84
tedmorris <31996410+tedmorris@users.noreply.github.com>
2018-05-24 11:30:16 -0700

    modify column mapping for RCRA_HD_EPISODIC_WASTE_CODE table

--- common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
+++ common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "4.0.11.1237";
+        public const string cAssemblyVersion = "4.0.11.1239";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
