ccbf2dcbd95ee3892cee79c36c692bc9810b3c07
tedmorris <31996410+tedmorris@users.noreply.github.com>
2018-08-17 12:52:37 -0700

    version update

--- common-windsor-net/windsor-commons-assemblyinfo/_AssemblyInfo.cs
+++ common-windsor-net/windsor-commons-assemblyinfo/_AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "4.0.14.$WCREV$";
+        public const string cAssemblyVersion = "4.0.15.$WCREV$";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
