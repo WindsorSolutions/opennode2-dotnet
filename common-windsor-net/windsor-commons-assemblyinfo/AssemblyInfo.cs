b2a8c7d85cee01475320f95fc21f8cc3b388d9da
tedmorris <31996410+tedmorris@users.noreply.github.com>
2018-12-05 16:13:01 -0800

    AFX updates

--- common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
+++ common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "4.0.15.1257";
+        public const string cAssemblyVersion = "4.0.15.1258";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
