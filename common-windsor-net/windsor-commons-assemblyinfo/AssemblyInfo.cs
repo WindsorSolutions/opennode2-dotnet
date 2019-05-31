eece56f69c6c14b0c699730caafb842147d6c8ca
tedmorris <31996410+tedmorris@users.noreply.github.com>
2019-05-30 18:28:01 -0700

    RCRA v5.7 initial sql

--- common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
+++ common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "4.0.15.1269";
+        public const string cAssemblyVersion = "4.0.15.1270";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
