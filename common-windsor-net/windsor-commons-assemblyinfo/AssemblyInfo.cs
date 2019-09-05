47ab632baafabba98f1a8c7da3c417076a363661
tedmorris <31996410+tedmorris@users.noreply.github.com>
2019-09-05 10:06:22 -0700

    new ICIS MappingMapToSubmissionResults.xslt

--- common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
+++ common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "4.0.16.1275";
+        public const string cAssemblyVersion = "4.0.16.1277";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
