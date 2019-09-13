f944f41525cfb0f260c4aeede98e920c9d8bbaa5
tedmorris <31996410+tedmorris@users.noreply.github.com>
2019-09-13 15:14:14 -0700

    new MappingMapToSubmissionResults.xslt

--- common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
+++ common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
@@ -44,7 +44,7 @@ namespace Windsor.Commons.AssemblyInfo
     internal static class AssemblyInfo
     {
         // [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-        public const string cAssemblyVersion = "4.0.16.1277";
+        public const string cAssemblyVersion = "4.0.16.1278";
 
         // [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
         public const string cAssemblyFileVersion = cAssemblyVersion;
