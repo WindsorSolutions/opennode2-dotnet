7fc51d48eb7b65e07403716cb905df82fcab53af
Ted <ted_morris@windsorsolutions.com>
2020-04-27 15:12:45 -0600

    version update

--- common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
+++ common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
@@ -48,8 +48,8 @@ namespace Windsor.Commons.AssemblyInfo
 			cAssemblyRevisionNumber = int.Parse(cAssemblyRevisionString);
 		}
 
-		public const string cAssemblyRevisionString = "1281";
-		public const string cAssemblyRevisionId = "a6a35f497e";
+		public const string cAssemblyRevisionString = "1282";
+		public const string cAssemblyRevisionId = "b70ab4d525";
 
 		public static readonly int cAssemblyRevisionNumber;
 
