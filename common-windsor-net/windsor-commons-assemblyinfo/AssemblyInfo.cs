22ed5871aedbf3f3bba739ec72c11399580d9a78
Ted <ted_morris@windsorsolutions.com>
2021-09-22 12:11:39 -0600

    rcra 511 start

--- common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
+++ common-windsor-net/windsor-commons-assemblyinfo/AssemblyInfo.cs
@@ -48,8 +48,8 @@ namespace Windsor.Commons.AssemblyInfo
 			cAssemblyRevisionNumber = int.Parse(cAssemblyRevisionString);
 		}
 
-		public const string cAssemblyRevisionString = "1326";
-		public const string cAssemblyRevisionId = "2e2cbff64a";
+		public const string cAssemblyRevisionString = "1327";
+		public const string cAssemblyRevisionId = "817b0096d3";
 
 		public static readonly int cAssemblyRevisionNumber;
 
