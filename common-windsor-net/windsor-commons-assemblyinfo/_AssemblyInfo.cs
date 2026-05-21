1d434d74c22557b54f7cd6c8e60531dc6f3dbc7f
Ted <ted_morris@windsorsolutions.com>
2026-05-21 13:24:55 -0600

    icis

--- common-windsor-net/windsor-commons-assemblyinfo/_AssemblyInfo.cs
+++ common-windsor-net/windsor-commons-assemblyinfo/_AssemblyInfo.cs
@@ -54,7 +54,7 @@ namespace Windsor.Commons.AssemblyInfo
 		public static readonly int cAssemblyRevisionNumber;
 
 		// [assembly: AssemblyVersion(AssemblyInfoServer.cAssemblyVersion)]
-		public const string cAssemblyVersion = "4.0.38." + cAssemblyRevisionString;
+		public const string cAssemblyVersion = "4.0.39." + cAssemblyRevisionString;
 
 		// [assembly: AssemblyFileVersion(AssemblyInfoServer.cAssemblyFileVersion)]
 		public const string cAssemblyFileVersion = cAssemblyVersion;
@@ -69,7 +69,7 @@ namespace Windsor.Commons.AssemblyInfo
 		public const string cAssemblyProduct = "OpenNode2";
 
 		// [assembly: AssemblyCopyright(AssemblyInfoServer.cAssemblyCopyright)]
-		public const string cAssemblyCopyrightDates = "(c) 2008-2025";
+		public const string cAssemblyCopyrightDates = "(c) 2008-2026";
 
 		// [assembly: AssemblyCopyright(AssemblyInfoServer.cAssemblyCopyright)]
 		public const string cAssemblyCopyright = cAssemblyCopyrightDates + " Windsor Solutions. All Rights Reserved.";
